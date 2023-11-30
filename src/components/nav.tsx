2260a4523ba804bb5c72fcc753ae6d0336ec16d2
awpoch <143761409+awpoch@users.noreply.github.com>
2023-11-30 15:59:56 -0700

    nav update

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -17,43 +17,43 @@ const Nav: FC<Props> = memo(({projectsList: li, backgroundHeight: backgroundHeig
 
     // All variables are being used and assigned  dynamically!
     const [isHoverReturn, setIsHoverReturn] = useState(false);
-    const [_isHover0, setIsHover0] = useState(false);
-    const [_isHover1, setIsHover1] = useState(false);
-    const [_isHover2, setIsHover2] = useState(false);
-    const [_isHover3, setIsHover3] = useState(false);
-    const [_isHover4, setIsHover4] = useState(false);
-    const [_isHover5, setIsHover5] = useState(false);
-    const [_isHover6, setIsHover6] = useState(false);
-    const [_isHover7, setIsHover7] = useState(false);
-    const [_isHover8, setIsHover8] = useState(false);
-    const [_isHover9, setIsHover9] = useState(false);
-    const [_isHover10, setIsHover10] = useState(false);
+    const [isHover0, setIsHover0] = useState(false);
+    const [isHover1, setIsHover1] = useState(false);
+    const [isHover2, setIsHover2] = useState(false);
+    const [isHover3, setIsHover3] = useState(false);
+    const [isHover4, setIsHover4] = useState(false);
+    const [isHover5, setIsHover5] = useState(false);
+    const [isHover6, setIsHover6] = useState(false);
+    const [isHover7, setIsHover7] = useState(false);
+    const [isHover8, setIsHover8] = useState(false);
+    const [isHover9, setIsHover9] = useState(false);
+    const [isHover10, setIsHover10] = useState(false);
 
     const handleMouseEnterReturn = () => { setIsHoverReturn(true); };
-    const _handleMouseEnter0 = () => { if(li[0][3] == "not selected") setIsHover0(true); };
-    const _handleMouseEnter1 = () => { if(li[1][3] == "not selected") setIsHover1(true); };
-    const _handleMouseEnter2 = () => { if(li[2][3] == "not selected") setIsHover2(true); };
-    const _handleMouseEnter3 = () => { if(li[3][3] == "not selected") setIsHover3(true); };
-    const _handleMouseEnter4 = () => { if(li[4][3] == "not selected") setIsHover4(true); };
-    const _handleMouseEnter5 = () => { if(li[5][3] == "not selected") setIsHover5(true); };
-    const _handleMouseEnter6 = () => { if(li[6][3] == "not selected") setIsHover6(true); };
-    const _handleMouseEnter7 = () => { if(li[7][3] == "not selected") setIsHover7(true); };
-    const _handleMouseEnter8 = () => { if(li[8][3] == "not selected") setIsHover8(true); };
-    const _handleMouseEnter9 = () => { if(li[9][3] == "not selected") setIsHover9(true); };
-    const _handleMouseEnter10 = () => { if(li[10][3] == "not selected") setIsHover10(true); };
+    const handleMouseEnter0 = () => { if(li[0][3] == "not selected") setIsHover0(true); };
+    const handleMouseEnter1 = () => { if(li[1][3] == "not selected") setIsHover1(true); };
+    const handleMouseEnter2 = () => { if(li[2][3] == "not selected") setIsHover2(true); };
+    const handleMouseEnter3 = () => { if(li[3][3] == "not selected") setIsHover3(true); };
+    const handleMouseEnter4 = () => { if(li[4][3] == "not selected") setIsHover4(true); };
+    const handleMouseEnter5 = () => { if(li[5][3] == "not selected") setIsHover5(true); };
+    const handleMouseEnter6 = () => { if(li[6][3] == "not selected") setIsHover6(true); };
+    const handleMouseEnter7 = () => { if(li[7][3] == "not selected") setIsHover7(true); };
+    const handleMouseEnter8 = () => { if(li[8][3] == "not selected") setIsHover8(true); };
+    const handleMouseEnter9 = () => { if(li[9][3] == "not selected") setIsHover9(true); };
+    const handleMouseEnter10 = () => { if(li[10][3] == "not selected") setIsHover10(true); };
 
     const handleMouseLeaveReturn = () => { setIsHoverReturn(false); };
-    const _handleMouseLeave0 = () => { if(li[0][3] == "not selected") setIsHover0(false); };
-    const _handleMouseLeave1 = () => { if(li[1][3] == "not selected") setIsHover1(false); };
-    const _handleMouseLeave2 = () => { if(li[2][3] == "not selected") setIsHover2(false); };
-    const _handleMouseLeave3 = () => { if(li[3][3] == "not selected") setIsHover3(false); };
-    const _handleMouseLeave4 = () => { if(li[4][3] == "not selected") setIsHover4(false); };
-    const _handleMouseLeave5 = () => { if(li[5][3] == "not selected") setIsHover5(false); };
-    const _handleMouseLeave6 = () => { if(li[6][3] == "not selected") setIsHover6(false); };
-    const _handleMouseLeave7 = () => { if(li[7][3] == "not selected") setIsHover7(false); };
-    const _handleMouseLeave8 = () => { if(li[8][3] == "not selected") setIsHover8(false); };
-    const _handleMouseLeave9 = () => { if(li[9][3] == "not selected") setIsHover9(false); };
-    const _handleMouseLeave10 = () => { if(li[10][3] == "not selected") setIsHover10(false); };
+    const handleMouseLeave0 = () => { if(li[0][3] == "not selected") setIsHover0(false); };
+    const handleMouseLeave1 = () => { if(li[1][3] == "not selected") setIsHover1(false); };
+    const handleMouseLeave2 = () => { if(li[2][3] == "not selected") setIsHover2(false); };
+    const handleMouseLeave3 = () => { if(li[3][3] == "not selected") setIsHover3(false); };
+    const handleMouseLeave4 = () => { if(li[4][3] == "not selected") setIsHover4(false); };
+    const handleMouseLeave5 = () => { if(li[5][3] == "not selected") setIsHover5(false); };
+    const handleMouseLeave6 = () => { if(li[6][3] == "not selected") setIsHover6(false); };
+    const handleMouseLeave7 = () => { if(li[7][3] == "not selected") setIsHover7(false); };
+    const handleMouseLeave8 = () => { if(li[8][3] == "not selected") setIsHover8(false); };
+    const handleMouseLeave9 = () => { if(li[9][3] == "not selected") setIsHover9(false); };
+    const handleMouseLeave10 = () => { if(li[10][3] == "not selected") setIsHover10(false); };
 
     const openClose = () => {
         if (window === false) {
@@ -132,12 +132,12 @@ const Nav: FC<Props> = memo(({projectsList: li, backgroundHeight: backgroundHeig
                                     marginTop: "15px",
                                     height: "43px"}}>
                                     <Image alt={item[1]} height={43} src={item[1]} width={43} />
-                                    <li onMouseEnter={eval("_handleMouseEnter" + i.toString())} onMouseLeave={eval("_handleMouseLeave" + i.toString())}style={{
+                                    <li onMouseEnter={eval("handleMouseEnter" + i.toString())} onMouseLeave={eval("handleMouseLeave" + i.toString())}style={{
                                         display: window === false ? "none" : "inline-block",
                                         padding: "0px 0px 0px 10px",
                                         position: "relative",
                                         top: "2.5px",
-                                        color: item[3] === "selected" ? "black" : eval("_isHover" + i.toString()) === false ? "blue" : "darkblue",
+                                        color: item[3] === "selected" ? "black" : eval("isHover" + i.toString()) === false ? "blue" : "darkblue",
                                         fontSize: "16px",
                                         letterSpacing: "0px",
                                         lineHeight: "38px",
